bc4b102a5a80cd5fb68b57a808ad719d864c53de
Christofer Koch <cbkoch13@gmail.com>
2025-01-15 13:32:48 -0800

    feat: add basic headers and while loop

--- ckmacs.c
+++ ckmacs.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
 
 int main()
 {
+  char c;
+
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'c');
+  
   return 0;
 }
