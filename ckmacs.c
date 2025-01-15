8ecb45c95407adfb6befcfc22f733d4bd006fed4
Christofer Koch <cbkoch13@gmail.com>
2025-01-15 13:56:52 -0800

    feat: disable canonical mode

--- ckmacs.c
+++ ckmacs.c
@@ -3,12 +3,29 @@
 #include <unistd.h>
 #include <termios.h>
 
+struct termios terminal_attr;
+
+void disable_raw_mode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminal_attr);
+}
+
+void enable_raw_mode()
+{
+  tcgetattr(STDIN_FILENO, &terminal_attr);
+  atexit(disable_raw_mode);
+
+  struct termios raw = terminal_attr;
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
   char c;
+  enable_raw_mode();
 
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'c');
-  
+
   return 0;
 }
