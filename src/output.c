f9f77efc3925e1c01ea4e548ee165ab72b1e9acb
Christofer Koch <cbkoch13@gmail.com>
2025-01-17 10:39:02 -0800

    misc: commit before switch

--- src/output.c
+++ src/output.c
@@ -1,12 +1,19 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <curses.h>
 
 WINDOW *win;
 
+void free_window()
+{
+  endwin();
+}
+
 void initialize_window()
 {
   initscr();
   win = newwin(0, 0, 0, 0);
+  atexit(free_window);
 }
 
 void refresh_screen()
